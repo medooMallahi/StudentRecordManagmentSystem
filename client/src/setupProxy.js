81df710c5b223f649fc5fba11473f026de724d0b
medooMallahi <almallahi@outlook.com>
2021-01-03 15:19:41 +0200

    change 4

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,31 +2,31 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api/students", { target: "http://localhost:7000/" })
+    createProxyMiddleware("/api/students", { target: "http://localhost:5400/" })
   );
   app.use(
     createProxyMiddleware("/api/getStudentInformation", {
-      target: "http://localhost:7000/",
+      target: "http://localhost:5400/",
     })
   );
   app.use(
     createProxyMiddleware("/api/getStudentCourses", {
-      target: "http://localhost:7000/",
+      target: "http://localhost:5400/",
     })
   );
   app.use(
     createProxyMiddleware("/api/addStudentCourse", {
-      target: "http://localhost:7000/",
+      target: "http://localhost:5400/",
     })
   );
   app.use(
     createProxyMiddleware("/api/users/logIn", {
-      target: "http://localhost:7000/",
+      target: "http://localhost:5400/",
     })
   );
   app.use(
     createProxyMiddleware("/api/users/register", {
-      target: "http://localhost:7000/",
+      target: "http://localhost:5400/",
     })
   );
 };
